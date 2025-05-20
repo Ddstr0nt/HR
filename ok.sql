444db1bfb6b636e82c44e0ec6aa9a18e7c8c40d9
Dead_Astr0naut <ddstr0nt@gmail.com>
2025-05-20 14:24:18 +0300

    `Added AUTO_INCREMENT to id fields in education, position, prof, and workers tables, and added foreign key constraints to workers table.`

--- ok.sql
+++ ok.sql
@@ -47,10 +47,10 @@ DROP TABLE IF EXISTS `education`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `education` (
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -102,8 +102,10 @@ CREATE TABLE `list` (
   `date` date DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_st_actions_idx` (`actions_id`),
-  CONSTRAINT `fk_l_actions` FOREIGN KEY (`actions_id`) REFERENCES `actions` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `fk_l_workers_idx` (`workers_id`),
+  CONSTRAINT `fk_l_actions` FOREIGN KEY (`actions_id`) REFERENCES `actions` (`id`),
+  CONSTRAINT `fk_l_workers` FOREIGN KEY (`workers_id`) REFERENCES `workers` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -112,7 +114,7 @@ CREATE TABLE `list` (
 
 LOCK TABLES `list` WRITE;
 /*!40000 ALTER TABLE `list` DISABLE KEYS */;
-INSERT INTO `list` VALUES (1,11,1,4,'2024-12-30'),(2,12,1,8,'2025-01-16'),(3,11,1,39,'2025-01-09'),(4,12,2,3,'2025-01-15'),(5,12,1,6,'2025-01-10');
+INSERT INTO `list` VALUES (1,21,1,5000,'2021-06-09'),(2,26,2,1000,'2015-06-11'),(3,26,1,3000,'2022-07-15'),(6,22,1,3000,'2018-09-30'),(7,24,2,200011,NULL),(8,24,2,1000,'2020-07-10'),(9,26,2,2000,'2018-11-13'),(17,26,1,12000,'2025-02-20'),(20,23,1,12223,'2025-05-05');
 /*!40000 ALTER TABLE `list` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -124,10 +126,10 @@ DROP TABLE IF EXISTS `position`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `position` (
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=37 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -136,7 +138,7 @@ CREATE TABLE `position` (
 
 LOCK TABLES `position` WRITE;
 /*!40000 ALTER TABLE `position` DISABLE KEYS */;
-INSERT INTO `position` VALUES (1,'555'),(2,'klkk');
+INSERT INTO `position` VALUES (1,'Разработчик'),(4,'Бухгалтер'),(26,'Системный администратор'),(27,'Старший бухгалтер '),(28,'Помощник бухгалтера'),(29,'HR-менеджер'),(30,'Инженер-конструктор'),(31,'Финансовый аналитик'),(32,'Юридический консультант'),(33,'Инженер-проектировщик');
 /*!40000 ALTER TABLE `position` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -148,10 +150,10 @@ DROP TABLE IF EXISTS `prof`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `prof` (
-  `id` int NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -160,7 +162,7 @@ CREATE TABLE `prof` (
 
 LOCK TABLES `prof` WRITE;
 /*!40000 ALTER TABLE `prof` DISABLE KEYS */;
-INSERT INTO `prof` VALUES (1,'Инженер'),(2,'Менеджер');
+INSERT INTO `prof` VALUES (1,'Инженер'),(2,'Программист'),(3,'Менеджер'),(6,'Экономист'),(7,'Юрист ');
 /*!40000 ALTER TABLE `prof` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -172,7 +174,7 @@ DROP TABLE IF EXISTS `workers`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `workers` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(45) DEFAULT NULL,
   `fam` varchar(45) DEFAULT NULL,
   `otch` varchar(45) DEFAULT NULL,
@@ -186,14 +188,14 @@ CREATE TABLE `workers` (
   `date_fired` date DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `fk_st_gender_idx` (`gender_id`),
-  KEY `fk_st_prof_idx` (`prof_id`),
-  KEY `fk_st_education_idx` (`education_id`),
   KEY `fk_w_position_idx` (`position_id`),
+  KEY `fk_w_prof_idx` (`prof_id`),
+  KEY `fk_w_education_idx` (`education_id`),
   CONSTRAINT `fk_w_education` FOREIGN KEY (`education_id`) REFERENCES `education` (`id`),
   CONSTRAINT `fk_w_gender` FOREIGN KEY (`gender_id`) REFERENCES `gender` (`id`),
   CONSTRAINT `fk_w_position` FOREIGN KEY (`position_id`) REFERENCES `position` (`id`),
   CONSTRAINT `fk_w_prof` FOREIGN KEY (`prof_id`) REFERENCES `prof` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=31 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -202,7 +204,7 @@ CREATE TABLE `workers` (
 
 LOCK TABLES `workers` WRITE;
 /*!40000 ALTER TABLE `workers` DISABLE KEYS */;
-INSERT INTO `workers` VALUES (11,'Иван','Иванов','Иванович',2,2,1,13,3,'2024-12-30','2025-01-06','2025-01-06'),(12,'ААА','ААА','АА',1,1,1,7,1,'2025-01-06','2025-01-06','2025-01-06');
+INSERT INTO `workers` VALUES (21,'Иван','Иванов','Иванович',1,1,30,80000,2,'1985-02-15','2010-05-01',NULL),(22,'Анна','Петрова','Сергеевна',2,6,27,75000,2,'1985-03-28','2012-01-15',NULL),(23,'Алексей','Сидоров','Петрович',1,2,26,120000,2,'1992-06-11','2012-03-01',NULL),(24,'Екатерина','Козлова','Александровна',2,3,29,90000,2,'1988-08-10','2013-08-22',NULL),(25,'Дмитрий','Смирнов','Владимирович',1,7,32,70000,2,'1982-05-05','2011-11-01','2022-12-31'),(26,'Дмитрий','Васильев','Андреевич',1,1,33,70000,2,'1985-06-13','2013-10-09',NULL),(27,'Андрей','Ткачев','Владимирович',1,6,28,50000,1,'1994-02-14','2022-01-04',NULL),(29,'Евгений','Воробьев','Иванович',1,6,31,70000,2,'1981-01-22','2020-10-23',NULL);
 /*!40000 ALTER TABLE `workers` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -215,4 +217,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-01-23 14:14:27
+-- Dump completed on 2025-05-20 14:14:33
